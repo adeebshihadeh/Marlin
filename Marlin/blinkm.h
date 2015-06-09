92cdcce5cf0f55f310173940249a1a1b14d10cc5
quillford <adeebshihadeh@gmail.com>
2015-06-09 01:06:32 -0700

    switched minimax to release branch

--- Marlin/blinkm.h
+++ Marlin/blinkm.h
@@ -1,9 +1,14 @@
 /*
-  blinkm.h
+  BlinkM.h
   Library header file for BlinkM library
  */
+#if (ARDUINO >= 100)
+  # include "Arduino.h"
+#else
+  # include "WProgram.h"
+#endif
 
-#include "Arduino.h"
 #include "Wire.h"
 
 void SendColors(byte red, byte grn, byte blu);
+
